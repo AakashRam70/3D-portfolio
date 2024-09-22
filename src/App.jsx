606cc2ef1a1790602770e318c6c911db6f7653c5
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-09-22 14:47:12 +0530

    adding Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return <div>
-    <section><a href="">Hello</a></section>
+    <section>
+      <Navbar />
+    </section>
     <section>Parallax</section>
     <section>Services</section>
     <section>Parallax</section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,15 @@ import "./navbar.scss"
 
 const Navbar = () => {
     return (
-        <div className='navbar'>Navbar</div>
+        <div className='navbar'>
+            {/* Sidebar */}
+            <div className="wrapper">
+                <span>Aakash Rambhas-d</span>
+                <div className="social">
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Navbar
+export default Navbar;
