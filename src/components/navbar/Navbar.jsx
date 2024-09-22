046d66dc0745ab070e0ad88d37727e59b1571f4b
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-09-22 18:51:15 +0530

    Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,15 @@ const Navbar = () => {
         <div className='navbar'>
             {/* Sidebar */}
             <div className="wrapper">
-                <span>Aakash Rambhas-d</span>
+                <span>Aakash Rambhad</span>
                 <div className="social">
+                    <a href="#"><img src="/facebook.png" alt="" /></a>
+                    <a href="#"><img src="/instagram.png" alt="" /></a>
+                    <a href="#"><img src="/youtube.png" alt="" /></a>
+                    <a href="#"><img src="/dribble.png" alt="" /></a>
                 </div>
             </div>
         </div>
     )
 }
-
 export default Navbar;
