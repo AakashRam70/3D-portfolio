ea0a2dbd2199dbfa530f302985d550a26af02a58
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-09-22 18:53:05 +0530

    Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                     <a href="#"><img src="/facebook.png" alt="" /></a>
                     <a href="#"><img src="/instagram.png" alt="" /></a>
                     <a href="#"><img src="/youtube.png" alt="" /></a>
-                    <a href="#"><img src="/dribble.png" alt="" /></a>
+                    <a href="#"><img src="/dribbble.png" alt="" /></a>
                 </div>
             </div>
         </div>
