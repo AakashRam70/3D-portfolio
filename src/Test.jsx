742303f3e6c2e08d524ef948f708ed328c4316a0
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-09-26 21:52:03 +0530

    adding code Test.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -6,21 +6,19 @@ const Test = () => {
     const [open, setOpen] = useState(false)
 
     const variants = {
-        visible: { opacity: 1 },
+        visible: { opacity: 1, x: 100, transition: { staggerChildren: 0.2 } },
         hidden: { opacity: 0 },
     }
 
+    const items = ["item1", "item2", "item3", "item4"];
+
     return (
         <div className="course">
-            <motion.div
-                className="box"
-                variants={variants}
-                // initial="hidden"
-                // animate="visible"
-                transition={{ duration: 2 }}
-                animate={open ? "visible" : "hidden"}
-            ></motion.div>
-            <button onClick={() => setOpen(prev => !prev)}>Click</button>
+            <motion.ul initial="hidden" animate="visible" variants={variants}>
+                {items.map((item) => (
+                    <motion.li variants={variants} key={item}>{item}</motion.li>
+                ))}
+            </motion.ul>
         </div>
     )
 }
