580ad2999ddb6fb061b53669cf621032af7918f0
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-09-30 21:52:26 +0530

    modifying Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -14,7 +14,6 @@ const Sidebar = () => {
                 restDelta: 2
             }
         },
-        
         closed: {
             clipPath: "circle(30px at 50px 50px)",
             transition: {
