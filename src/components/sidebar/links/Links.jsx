97ead4156bfebf096ca4c2678dcdca7bf80074d9
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-01 10:18:40 +0530

    modifying Links.jsx

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,15 +1,38 @@
-const variants = {
+import { motion } from "framer-motion";
 
+const variants = {
+    open: {
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+    closed: {
+        transition: {
+            staggerChildren: 0.05,
+            staggerDirection: -1,
+        },
+    },
+}
+
+const itemVariants = {
+    open: {
+        y: 0,
+        opacity: 1,
+    },
+    closed: {
+        y: 50,
+        opacity: 0,
+    },
 }
 
 const Links = () => {
     const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
     return (
-        <div className='links'>{items.map((item) => (
+        <motion.div className='links' variants={itemVariants}>{items.map((item) => (
             <a href={`#${item}`} key={item}>
                 {item}
             </a>
-        ))}</div>
+        ))}</motion.div>
     )
 }
 
