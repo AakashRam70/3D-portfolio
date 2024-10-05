dd3838c94d935f2c22592334d708e64dc7b69558
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-05 16:29:00 +0530

    modifying Hero.jsx and hero.scss

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,10 @@ import "./hero.scss";
 const Hero = () => {
     return (
         <div className="hero">
+            <div className="textContainer">
+                <h2>AAKASH RAMBHAD</h2>
+                <h1>Web developer and UI designer</h1>
+            </div>
             <div className="imageContainer">
                 <img src="/hero.png" alt="" />
             </div>
