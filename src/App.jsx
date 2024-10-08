a9e3debde3ab1dc6ee1907ba4f1004a8f0531071
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-08 13:11:32 +0530

    adding new file Parallax.jsx and parallax.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Test from "./Test";
+import Parallax from "./components/parallax/Parallax";
+import Parllax from "./components/parallax/Parallax";
 const App = () => {
   return <div>
     <section>
       <Navbar />
       <Hero />
     </section>
-    <section>Parallax</section>
+    <section id="Services"><Parllax type="services" /></section>
     <section>Services</section>
-    <section>Parallax</section>
+    <section id="Portfolio"><Parallax type="portfolio" /></section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
     <section>Contact</section>
