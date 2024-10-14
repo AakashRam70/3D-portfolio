1b10e6f536fb022aee733a5cd7921681e6d16b35
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-14 23:52:38 +0530

    modifying Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,20 +1,35 @@
 import './services.scss'
 import { motion } from 'framer-motion';
+
+const variants = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+}
 const Services = () => {
     return (
-        <motion.div className="services">
-            <motion.div className='textContainer'><p>I focus on helping your brand grow <br /> and move forward</p><hr /></motion.div>
-            <motion.div className='titleContainer'>
-                <div className="title">
+        <motion.div className="services" variants={variants} initial="initial" animate="animate">
+            <motion.div className='textContainer' variants={variants}><p>I focus on helping your brand grow <br /> and move forward</p><hr /></motion.div>
+            <motion.div className='titleContainer' variants={variants}>
+                <motion.div className="title">
                     <img src="/people.webp" alt="" />
                     <h1><b>Unique</b>Ideas</h1>
-                </div>
-                <div className="title">
+                </motion.div>
+                <motion.div className="title">
                     <h1><b>For Your</b>Business.</h1>
                     <button>WHAT WE DO?</button>
-                </div>
+                </motion.div>
             </motion.div>
-            <motion.div className='listContainer'>
+            <motion.div className='listContainer' variants={variants}>
                 <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sint, perspiciatis repellat quidem ullam quas totam eveniet ab quia harum illo praesentium maxime ipsa, reiciendis ea nulla suscipit magni voluptatibus corporis nisi qui!</p>
