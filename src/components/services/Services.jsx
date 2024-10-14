fff688f89713cbd0e96be1a9605ed53d223c87fe
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-14 22:43:24 +0530

    modifying Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import './services.scss'
 import { motion } from 'framer-motion';
 const Services = () => {
     return (
         <motion.div className="services">
             <motion.div className='textContainer'><p>I focus on helping your brand grow <br /> and move forward</p><hr /></motion.div>
-            <motion.div className='titleContainer'></motion.div>
+            <motion.div className='titleContainer'>
+                <div className="title">
+                    <img src="/people.png" alt="" />
+                </div>
+            </motion.div>
             <motion.div className='listContainer'></motion.div>
         </motion.div>
     )
