7ed098580a4a2dd04707483f29ed1f13e863ad0b
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-14 23:00:24 +0530

    modifying Services.scss

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,10 +6,36 @@ const Services = () => {
             <motion.div className='textContainer'><p>I focus on helping your brand grow <br /> and move forward</p><hr /></motion.div>
             <motion.div className='titleContainer'>
                 <div className="title">
-                    <img src="/people.png" alt="" />
+                    <img src="/people.webp" alt="" />
+                    <h1><b>Unique</b>Ideas</h1>
+                </div>
+                <div className="title">
+                    <h1><b>For Your</b>Business.</h1>
+                    <button>WHAT WE DO?</button>
                 </div>
             </motion.div>
-            <motion.div className='listContainer'></motion.div>
+            <motion.div className='listContainer'>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sint, perspiciatis repellat quidem ullam quas totam eveniet ab quia harum illo praesentium maxime ipsa, reiciendis ea nulla suscipit magni voluptatibus corporis nisi qui!</p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sint, perspiciatis repellat quidem ullam quas totam eveniet ab quia harum illo praesentium maxime ipsa, reiciendis ea nulla suscipit magni voluptatibus corporis nisi qui!</p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sint, perspiciatis repellat quidem ullam quas totam eveniet ab quia harum illo praesentium maxime ipsa, reiciendis ea nulla suscipit magni voluptatibus corporis nisi qui!</p>
+                    <button>Go</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque sint, perspiciatis repellat quidem ullam quas totam eveniet ab quia harum illo praesentium maxime ipsa, reiciendis ea nulla suscipit magni voluptatibus corporis nisi qui!</p>
+                    <button>Go</button>
+                </div>
+            </motion.div>
         </motion.div>
     )
 }
