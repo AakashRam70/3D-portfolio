68d2ae79bdb962d97bc1abd1df77c6fa2dd5c6ba
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-16 03:30:42 +0530

    Modifying Portfolio.jsx and portfolio.scss

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import "./portfolio.scss"
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
     {
@@ -30,9 +30,27 @@ const items = [
 ]
 
 const Single = ({ item }) => {
+
+    const ref = useRef()
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+    })
+
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
     return (
-        <section>
-            {item.title}
+        <section ref={ref}>
+            <div className="container">
+                <div className="wrapper">
+                    <img src={item.img} alt="" />
+                    <motion.div className="textContainer" style={{ y }}>
+                        <h2>{item.title}</h2>
+                        <p>{item.desc}</p>
+                        <button>See Demo</button>
+                    </motion.div>
+                </div>
+            </div>
         </section>
     )
 }
