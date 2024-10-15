5c0850634e00752941aef13945e7fdb8f6dd3154
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-16 03:16:19 +0530

    Modifying Portfolio.jsx and portfolio.scss

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./portfolio.scss"
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const items = [
     {
@@ -36,8 +37,24 @@ const Single = ({ item }) => {
     )
 }
 const Portfolio = () => {
+
+    const ref = useRef()
+
+    const { scrollYProgress } = useScroll({ target: ref, offset: ["end end", "start start"] });
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+    })
+
     return (
-        <div className='portfolio'>
+        <div className='portfolio' ref={ref}>
+            <div className="progress">
+                <h1>Feactured Works</h1>
+                <motion.div style={{ scaleX }} className="progressBar">
+
+                </motion.div>
+            </div>
             {items.map(item => (
                 <Single item={item} key={item.id} />
             ))}
