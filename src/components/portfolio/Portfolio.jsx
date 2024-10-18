5a1462a37a12902e6fc43c2a93c7bb6dbb7aa99e
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-19 01:04:22 +0530

    modifying Portfolio.jsx and portfolio.scss

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,25 +6,25 @@ const items = [
     {
         id: 1,
         title: "React Commerce",
-        img: "https://images.pexels.com/photos/28425084/pexels-photo-28425084/free-photo-of-silhouette-of-person-with-bicycle-under-starry-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        img: "https://images.pexels.com/photos/12467404/pexels-photo-12467404.jpeg?auto=compress&cs=tinysrgb&w=400",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ab asperiores assumenda tenetur atque recusandae, sunt hic nam voluptate beatae inventore dolor quod, blanditiis facilis ea expedita ex nulla at."
     },
     {
         id: 2,
         title: "Next Js Blog",
-        img: "https://www.pexels.com/video/the-green-water-is-surrounded-by-trees-and-green-vegetation-17382716/",
+        img: "https://images.pexels.com/photos/18463144/pexels-photo-18463144/free-photo-of-analog-camera-with-films.jpeg?auto=compress&cs=tinysrgb&w=400",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ab asperiores assumenda tenetur atque recusandae, sunt hic nam voluptate beatae inventore dolor quod, blanditiis facilis ea expedita ex nulla at."
     },
     {
         id: 3,
         title: "Javascript App",
-        img: "https://images.pexels.com/photos/28425084/pexels-photo-28425084/free-photo-of-silhouette-of-person-with-bicycle-under-starry-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=loadhttps://www.pexels.com/photo/embroidery-floral-on-tray-20409572/",
+        img: "https://images.pexels.com/photos/6506698/pexels-photo-6506698.jpeg?auto=compress&cs=tinysrgb&w=400",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ab asperiores assumenda tenetur atque recusandae, sunt hic nam voluptate beatae inventore dolor quod, blanditiis facilis ea expedita ex nulla at."
     },
     {
         id: 4,
         title: "Node JS",
-        img: "https://www.pexels.com/photo/white-and-pink-flower-petals-in-close-up-photography-12283023/",
+        img: "https://images.pexels.com/photos/20693451/pexels-photo-20693451/free-photo-of-canon-eos-slr-cameras.jpeg?auto=compress&cs=tinysrgb&w=400",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ab asperiores assumenda tenetur atque recusandae, sunt hic nam voluptate beatae inventore dolor quod, blanditiis facilis ea expedita ex nulla at."
     }
 ]
