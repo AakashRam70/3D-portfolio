1cb23f2cd5713156b7206c9d38567daf8e76b074
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-20 01:37:46 +0530

    Modifying file Contact.jsx and contact.scss

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,31 @@ import "./contact.scss"
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className="contact">
+            <div className="textContainer">
+                <h1>Let's Work together</h1>
+                <div className="item">
+                    <h2>Mail</h2>
+                    <span>hello@gmail.com</span>
+                </div>
+                <div className="item">
+                    <h2>Address</h2>
+                    <span>40, Somalwada Nagpur</span>
+                </div>
+                <div className="item">
+                    <h2>Phone</h2>
+                    <span>+915480365927</span>
+                </div>
+            </div>
+            <div className="formContainer">
+                <form>
+                    <input type="text" placeholder='name' />
+                    <input type="email" placeholder='Email' />
+                    <textarea rows={8} placeholder='Message'></textarea>
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
